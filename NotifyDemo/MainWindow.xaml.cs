73049731393e3024dedde2b3471f4aba1013bf5a
dluppoli <davide@davideluppoli.it>
2022-12-15 16:08:55 +0100

    Add project files.

---
+++ NotifyDemo/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotifyDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindowViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new MainWindowViewModel();
+            DataContext = vm;
+        }
+
+        private void LoadList_Click(object sender, RoutedEventArgs e)
+        {
+            vm.LoadList();
+        }
+        private void AddList_Click(object sender, RoutedEventArgs e)
+        {
+            vm.AddList();
+        }
+        private void ModList_Click(object sender, RoutedEventArgs e)
+        {
+            vm.ModObs();
+        }
+
+        private void LoadObs_Click(object sender, RoutedEventArgs e)
+        {
+            vm.LoadObs();
+        }
+        private void AddObs_Click(object sender, RoutedEventArgs e)
+        {
+            vm.AddObs();
+        }
+        private void ModObs_Click(object sender, RoutedEventArgs e)
+        {
+            vm.ModList();
+        }
+    }
+}
---
+++ NotifyDemo/MainWindowViewModel.cs
@@ -0,0 +1,97 @@
+﻿using NotifyDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyDemo
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region List
+        private List<User> _usersList;
+
+		public List<User> UsersList
+        {
+			get { return _usersList; }
+			set { _usersList = value; NotifyPropertyChanged("UsersList"); }
+		}
+
+		internal void AddList()
+		{
+            if (UsersList != null && UsersList.Count > 0)
+            {
+				UsersList.Add(new User { Username = "Carlo" });
+            }
+        }
+
+		internal void LoadList()
+		{
+			UsersList = new List<User>
+			{
+				new User { Username = "Mario" },
+				new User { Username = "Luigi" }
+			};
+		}
+
+		internal void ModList()
+		{
+			if( UsersList!= null && UsersList.Count > 0 )
+			{
+				UsersList.FirstOrDefault().Username += "_mod";
+			}
+		}
+        #endregion
+
+        #region ObservableCollection
+        private ObservableCollection<User> _usersObs;
+
+        public ObservableCollection<User> UsersObs
+        {
+            get { return _usersObs; }
+            set { _usersObs = value; NotifyPropertyChanged("UsersObs"); }
+        }
+
+        internal void AddObs()
+        {
+            if (UsersObs != null && UsersObs.Count > 0)
+            {
+                UsersObs.Add(new User { Username = "Carlo" });
+            }
+        }
+
+        internal void LoadObs()
+        {
+            UsersObs = new ObservableCollection<User>
+            {
+                new User { Username = "Mario" },
+                new User { Username = "Luigi" }
+            };
+            UsersObs.CollectionChanged += UsersObs_CollectionChanged;
+        }
+
+        internal void ModObs()
+        {
+            if (UsersObs != null && UsersObs.Count > 0)
+            {
+                UsersObs.FirstOrDefault().Username += "_mod";
+            }
+        }
+        #endregion
+    
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private void UsersObs_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
